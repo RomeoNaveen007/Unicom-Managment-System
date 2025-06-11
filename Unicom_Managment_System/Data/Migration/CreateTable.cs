ec653d447f7562c0a9d715060af8bb9bd53ef4ca
Raveenthiran Naveen <UT010057@unicomtic.com>
2025-06-11 16:20:56 +0530

    First commit

---
+++ Unicom_Managment_System/Data/Migration/CreateTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicom_Managment_System.Data.DB_Connection;
+
+namespace Unicom_Managment_System.Data.Migration
+{
+    internal class CreateTable
+    {
+        public void table_Creation()
+        {
+            using (var conn = DBconfig.getConnection())
+            {
+                string query = @"" ;
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+
+            }
+
+        }
+
+    }
+}
