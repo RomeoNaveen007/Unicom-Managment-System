b33835ec7a2712c1e37f2c132f71e39c35ca2256
RomeoNaveen007 <raveenthirannaveen7@gmail.com>
2025-06-14 07:26:44 +0530

    5th Commit

---
+++ Unicom_Managment_System/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicom_Managment_System.Model
+{
+    internal class User
+    {
+        public int User_ID { get; set; }
+        public string User_Name { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
